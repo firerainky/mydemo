c1d2676e9a6443bcd05ba102e9762876a3f6a8f3
zhengkanyan <zhengkanyan@zhejianglab.com>
2025-02-25 14:40:33 +0800

    Use config value in config file: application.properties

--- src/main/java/com/zky/springboot/mydemo/rest/HelloRestController.java
+++ src/main/java/com/zky/springboot/mydemo/rest/HelloRestController.java
@@ -1,5 +1,6 @@
 package com.zky.springboot.mydemo.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,9 +8,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloRestController {
 
+    @Value("${pos3.hero}")
+    private String pos3Hero;
+
     @GetMapping("/")
     public String salute() {
-        return "Hello World";
+        return pos3Hero;
     }
-    
 }
