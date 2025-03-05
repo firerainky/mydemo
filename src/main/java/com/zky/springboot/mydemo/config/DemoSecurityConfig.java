bb83866eaf1ce90dd6809e4440580474124356e8
zhengkanyan <zhengkanyan@zhejianglab.com>
2025-03-05 16:37:27 +0800

    Add authorization to the API with servlet filter

--- src/main/java/com/zky/springboot/mydemo/config/DemoSecurityConfig.java
+++ src/main/java/com/zky/springboot/mydemo/config/DemoSecurityConfig.java
@@ -2,9 +2,13 @@ package com.zky.springboot.mydemo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class DemoSecurityConfig {
@@ -24,5 +28,20 @@ public class DemoSecurityConfig {
                 .password("{noop}test123")
                 .roles("EMPLOYEE", "MANAGER", "ADMIN").build();
         return new InMemoryUserDetailsManager(john, mary, susan);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        // 'HttpSecurity' Lambda DSL syntax
+        http.authorizeHttpRequests(authz -> 
+                authz
+                    .requestMatchers(HttpMethod.GET, "/api/employees").hasRole("EMPLOYEE")
+                    .requestMatchers(HttpMethod.GET, "/api/employees/**").hasRole("EMPLOYEE")
+                    .requestMatchers(HttpMethod.POST, "/api/employees").hasRole("MANAGER")
+                    .requestMatchers(HttpMethod.PUT, "/api/employees").hasRole("MANAGER")
+                    .requestMatchers(HttpMethod.DELETE, "/api/employees/**").hasRole("ADMIN"));
+
+        http.httpBasic(Customizer.withDefaults());  // Basic authentication
+        return http.build();
     }
 }
