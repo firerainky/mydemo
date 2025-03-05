be74b852c5865ff91b4885cfdf8a573d6b9c32f7
zhengkanyan <zhengkanyan@zhejianglab.com>
2025-03-05 17:11:22 +0800

    Change in memory user auth to db user auth

--- src/main/java/com/zky/springboot/mydemo/config/DemoSecurityConfig.java
+++ src/main/java/com/zky/springboot/mydemo/config/DemoSecurityConfig.java
@@ -1,33 +1,40 @@
 package com.zky.springboot.mydemo.config;
 
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class DemoSecurityConfig {
 
+    // Add support for in-memory user
+    // @Bean
+    // public InMemoryUserDetailsManager userDetailsManager() {
+    //     UserDetails john = User.builder()
+    //             .username("john")
+    //             .password("{noop}test123")
+    //             .roles("EMPLOYEE").build();
+    //     UserDetails mary = User.builder()
+    //             .username("mary")
+    //             .password("{noop}test123")
+    //             .roles("EMPLOYEE", "MANAGER").build();
+    //     UserDetails susan = User.builder()
+    //             .username("susan")
+    //             .password("{noop}test123")
+    //             .roles("EMPLOYEE", "MANAGER", "ADMIN").build();
+    //     return new InMemoryUserDetailsManager(john, mary, susan);
+    // }
+
     @Bean
-    public InMemoryUserDetailsManager userDetailsManager() {
-        UserDetails john = User.builder()
-                .username("john")
-                .password("{noop}test123")
-                .roles("EMPLOYEE").build();
-        UserDetails mary = User.builder()
-                .username("mary")
-                .password("{noop}test123")
-                .roles("EMPLOYEE", "MANAGER").build();
-        UserDetails susan = User.builder()
-                .username("susan")
-                .password("{noop}test123")
-                .roles("EMPLOYEE", "MANAGER", "ADMIN").build();
-        return new InMemoryUserDetailsManager(john, mary, susan);
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
     }
 
     @Bean
