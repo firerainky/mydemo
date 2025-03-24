77378a5904da9ca6c0222ec35f7a4d02d315a537
zhengkanyan <zhengkanyan@zhejianglab.com>
2025-03-24 15:49:45 +0800

    Add tymeleaf code

--- src/main/java/com/zky/springboot/mydemo/rest/ThymeleafController.java
+++ src/main/java/com/zky/springboot/mydemo/rest/ThymeleafController.java
@@ -3,6 +3,7 @@ package com.zky.springboot.mydemo.rest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
@@ -11,7 +12,18 @@ public class ThymeleafController {
     @GetMapping("/")
     public String sayHello(Model model) {
         model.addAttribute("theDate", java.time.LocalDateTime.now());
-        return "helloworld"; // "helloworld.html"
+        return "time"; // "helloworld.html"
     }
     
+    @RequestMapping("/showForm")
+    public String showForm() {
+        return "helloworld-form";
+    }
+
+    @RequestMapping("/processForm")
+    public String processForm() {
+        return "helloworld";
+    }
 }
+
+
